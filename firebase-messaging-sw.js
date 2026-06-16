27b16afcdb07400f2a142d6b0f7484cdb65a0a09
anthonycollobert60-bit <anthony.collobert60@gmail.com>
2026-06-16 16:54:15 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,32 +1,32 @@
-/* Firebase Messaging Service Worker - PokeCards Collection */
-importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyDk8RyaXWt4cHLot3puOoAn_wF372pt_Qc",
   authDomain: "pokecards-collection.firebaseapp.com",
   projectId: "pokecards-collection",
   storageBucket: "pokecards-collection.firebasestorage.app",
   messagingSenderId: "890242521469",
   appId: "1:890242521469:web:1d533eadfa41a143d8d414"
-};
+});
 
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   const notification = payload.notification || {};
-  const title = notification.title || 'PokeCards Collection';
-  const options = {
-    body: notification.body || '',
-    icon: '/icon-192.png',
-    badge: '/icon-192.png',
-    data: payload.data || {}
-  };
-  self.registration.showNotification(title, options);
+
+  self.registration.showNotification(
+    notification.title || "PokeCards Collection",
+    {
+      body: notification.body || "",
+      icon: "/icon-192.png",
+      badge: "/icon-192.png",
+      data: payload.data || {}
+    }
+  );
 });
 
-self.addEventListener('notificationclick', (event) => {
+self.addEventListener("notificationclick", (event) => {
   event.notification.close();
-  event.waitUntil(clients.openWindow('/'));
+  event.waitUntil(clients.openWindow("/"));
 });
